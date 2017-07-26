53dba6ef6af89cb6f4f830feb377985666635841
carlos Mario <carlospuello03@gmail.com>
2017-07-25 22:19:13 -0500

    [#card-13] - logging

---
+++ modules/custom/card_13_loggin/src/Entity/Controller/NodeUpdate.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\card_13_loggin\Entity\Controller;
+
+use Drupal\Core\Entity\ContentEntityConfirmFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * Provides a form for deleting a content_entity_example entity.
+ *
+ * @ingroup content_entity_example
+ */
+class NodeUpdate extends ContentEntityConfirmFormBase {
+
+  /**
+   * {@inheritdoc}
+   *
+   * Delete the entity and log the event. logger() replaces the watchdog.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $entity = $this->getEntity();
+    print_r($entity);
+    die();
+    //$entity->delete();
+
+    $this->logger('content_entity_example')->notice('@type: deleted %title.',
+      array(
+        '@type' => $this->entity->bundle(),
+        '%title' => $this->entity->label(),
+      ));
+    $form_state->setRedirect('entity.content_entity_example_contact.collection');
+  }
+
+}
